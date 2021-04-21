f81018ae0be587bff8f3c1e28c71b71798ed2c45
jimmy gonzales <jimmy@jimmys-mbp.localdomain>
2021-04-21 02:57:35 +0200

    up dat bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Telegraf } = require('telegraf')
+
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply(':thumbsup:'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.command('rugme', (ctx) => ctx.reply("the presale link is " + 'https://app.bounce.finance/fixed-swap/5350' + " ser"))
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
