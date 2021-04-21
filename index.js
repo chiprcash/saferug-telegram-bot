08773212972282b8978038e6e7fe7577f949a081
jimmy gonzales <jimmy@jimmys-mbp.localdomain>
2021-04-21 03:57:10 +0200

    up dat 4

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const { Telegraf } = require('telegraf')
 var binder = require("heroku-bind-port")
 binder()
 
+var Units = require("ethereumjs-units")
+const fetch = require('node-fetch')
+
+
 require('dotenv').config()
+
+function valueBNB(val){
+    return Units.convert(val.toString(), "wei", "eth");
+}
 
 const bot = new Telegraf(process.env.TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
@@ -11,7 +19,14 @@ bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply(':thumbsup:'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.command('rugme', (ctx) => ctx.reply("the presale link is " + 'https://app.bounce.finance/fixed-swap/5350' + " ser"))
-bot.command('rugfunds', (ctx) => ctx.reply("people lost 0.73 BNB so far, ser"))
+
+bot.command('rugfunds', (ctx) => {
+    fetch('https://api.bscscan.com/api?module=account&action=balance&address=0x00c6efa79ccd7d184e207ea48f09726e89a33249&tag=latest')
+        .then(res => res.json())
+        .then(json => {
+            ctx.reply("people lost " + valueBNB(json.result) + " BNB so far, ser");
+        });
+})
 
 bot.launch()
 
